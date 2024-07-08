eb35dc0881f534025687ae4acc467d14e35b3e01
Paulina Izienicka <paulaiz876@gmail.com>
2024-07-08 23:46:12 +0200

    Writing data to an output file

--- project.py
+++ project.py
@@ -22,30 +22,44 @@ def validate_json(file_content):
         raise
 
 
+def convert_file(input_path, output_path):
+    input_format = os.path.splitext(input_path)[-1].lower()
+    output_format = os.path.splitext(output_path)[-1].lower()
+
+    with open(input_path, 'r', encoding='utf-8') as input_file:
+        input_content = input_file.read()
+
+        if input_format == '.xml':
+            data = validate_xml(input_content)
+        elif input_format == '.json':
+            data = validate_json(input_content)
+        else:
+            raise ValueError(f'Unsupported file extension {input_format} (Input file)')
+
+    with open(output_path, 'w', encoding='utf-8') as output_file:
+        if output_format == '.xml':
+            xml_data = xmltodict.unparse(data, pretty=True)
+            output_file.write(xml_data)
+        elif output_format == '.json':
+            json.dump(data, output_file, indent=4)
+        else:
+            raise ValueError(f'Unsupported file extension: {output_format} (Output file)')
+
+
 def main():
     if len(sys.argv) != 3:
         print("Use: 'project.exe pathFile1 pathFile2'")
         return
+
     try:
         input_path = sys.argv[1]
         output_path = sys.argv[2]
-
-        input_format = os.path.splitext(input_path)[-1].lower()
-        output_format = os.path.splitext(output_path)[-1].lower()
-
-        with open(input_path, 'r', encoding='utf-8') as input_file:
-            input_content = input_file.read()
-
-            if input_format == '.xml':
-                data = validate_xml(input_content)
-            elif input_format == '.json':
-                data = validate_json(input_content)
-            else:
-                raise ValueError(f'Unsupported file extension {input_format} (Input file)')
+        convert_file(input_path, output_path)
+        print(f'Successfully converted {input_path} to {output_path}')
     except FileNotFoundError as e:
         print(e)
     except Exception as e:
-        print(e)
+        print(f'Conversion failed: {e}')
 
 
 if __name__ == '__main__':
